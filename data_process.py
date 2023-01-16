f23212c7aa56ca2acf5970b6ed0603745b3c97d2
Enow Takang <84392819+Enowtakang@users.noreply.github.com>
2023-01-16 17:33:20 +0100

    Add files via upload

---
+++ data_process.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+
+# Load dataset
+data = pd.read_csv('ydata.csv')
+print(data.shape)
+
+# Drop the YEAR column
+data = data.drop('YEAR', axis=1)
+print(data.shape)
+
+# Save prepared data to a separate CSV file
+ydata_processed = data.to_csv(
+    'data.csv',
+    index=None)
